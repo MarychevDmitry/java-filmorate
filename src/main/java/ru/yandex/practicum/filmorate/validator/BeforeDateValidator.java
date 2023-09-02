950f9b75f09431ff8b8c22fddf6a9b16404816f9
MarychevDmitry <blackriwz@yandex.ru>
2023-09-02 18:20:22 +0300

    Реализован весь функционал описаный в тз.

---
+++ src/main/java/ru/yandex/practicum/filmorate/validator/BeforeDateValidator.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.filmorate.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class BeforeDateValidator implements ConstraintValidator<BeforeDate, LocalDate> {
+    private LocalDate date;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    @Override
+    public void initialize(BeforeDate constraintAnnotation) {
+        this.date = LocalDate.parse(constraintAnnotation.value(), formatter);
+    }
+
+    @Override
+    public boolean isValid(LocalDate target, ConstraintValidatorContext context) {
+        return !target.isBefore(date);
+    }
+}
