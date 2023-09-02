d28b495db8bde66d91b92c0e0651e155e5f158b5
MarychevDmitry <blackriwz@yandex.ru>
2023-09-02 21:36:58 +0300

    Реализован весь функционал описанный в ТЗ.

--- src/main/java/ru/yandex/practicum/filmorate/exception/UpdateException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/UpdateException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class UpdateException extends RuntimeException{
+public class UpdateException extends RuntimeException {
 
     public UpdateException(String message) {
         super(message);
